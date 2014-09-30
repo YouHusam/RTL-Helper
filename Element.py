af47921086f9da44f925c12a7ddd9f8faa92df6e
Husam Younis <youhusam@gmail.com>
2014-09-30 04:29:18 +0300

    rename the seperate function and add todos

--- Element.py
+++ Element.py
@@ -6,12 +6,19 @@ class Element:
 
   def __init__(self, cssLine):
     # .sd > ff {height: 40px; float: left; margin: 4px 4px 5px 10px; }
-    seperate(cssLine)
+    populate(cssLine)
 
-  def seperate(cssLine):
+  #TODO: find if property is relevant and populate the dictionary
+  def populate(cssLine):
     splittedLine = cssLine.strip('}').split('{')
     self.selector = splittedLine[0]
     propertiesSet = splittedLine[1]
     for prop in propertiesSet:
       relevantProperty = prop.split(';')
       self.properties[relevantProperty[0]] = relevantProperty[1]
+
+  #TODO
+  def checkValues(cssProperty):
+    """Checks if property is relevant by checking if it contains the following:
+       left, right, margin, padding, background, border, float"""
+    pass
