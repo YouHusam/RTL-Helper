a7040a09127297818f0a277e9347e11749328e35
Husam Younis <youhusam@gmail.com>
2014-10-01 01:44:32 +0300

    Add comments, main, and populate the dict

--- Element.py
+++ Element.py
@@ -1,24 +1,34 @@
 class Element:
-  """docstring for Element"""
-
-  selector = ""
-  properties = {}
+  """ This class is for a css element style, it will also determine whether or
+      not css properties are relevant. """
 
   def __init__(self, cssLine):
-    # .sd > ff {height: 40px; float: left; margin: 4px 4px 5px 10px; }
-    populate(cssLine)
+    self.selector = ""
+    self.properties = {}
+    self.populate(cssLine)
 
-  #TODO: find if property is relevant and populate the dictionary
-  def populate(cssLine):
+  def populate(self, cssLine):
+    """ Populate the properties dict with the relevant properties """
     splittedLine = cssLine.strip('}').split('{')
     self.selector = splittedLine[0]
-    propertiesSet = splittedLine[1]
+    propertiesSet = splittedLine[1].split(';')
+    keywords = ['left', 'right', 'margin', 'padding', 'border',
+     'background', 'float']
     for prop in propertiesSet:
-      relevantProperty = prop.split(';')
-      self.properties[relevantProperty[0]] = relevantProperty[1]
+      print(prop)
+      for kw in keywords:
+        """ Checks if property is relevant by checking if it contains the
+            following: left, right, margin, padding, background, border, float"""
+        if kw in prop:
+          relevantProperty = prop.split(':')
+          self.properties[relevantProperty[0].strip()] = relevantProperty[1].strip()
 
-  #TODO
-  def checkValues(cssProperty):
-    """Checks if property is relevant by checking if it contains the following:
-       left, right, margin, padding, background, border, float"""
-    pass
+
+
+def main():
+  El = Element('.sd > ff {height: 40px; float: left; margin: 4px 4px 5px 10px; }')
+  print(El.selector)
+  print(El.properties)
+
+if __name__ == '__main__':
+  main()
