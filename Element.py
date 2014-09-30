251d53c34795df30c0173de92cda30cba2567528
Husam Younis <youhusam@gmail.com>
2014-10-01 02:32:40 +0300

    change the names of some varialbles, and add __str__ method

--- Element.py
+++ Element.py
@@ -1,34 +1,38 @@
 class Element:
-  """ This class is for a css element style, it will also determine whether or
+  """ This class is for a css element style. It will also determine whether or
       not css properties are relevant. """
 
-  def __init__(self, cssLine):
+  def __init__(self, rule):
     self.selector = ""
     self.properties = {}
-    self.populate(cssLine)
+    self.populate(rule)
 
-  def populate(self, cssLine):
+  def populate(self, rule):
     """ Populate the properties dict with the relevant properties """
-    splittedLine = cssLine.strip('}').split('{')
-    self.selector = splittedLine[0]
-    propertiesSet = splittedLine[1].split(';')
+    splittedRule = rule.strip('}').split('{')
+    self.selector = splittedRule[0]
+    propertiesList = splittedRule[1].split(';')
     keywords = ['left', 'right', 'margin', 'padding', 'border',
      'background', 'float']
-    for prop in propertiesSet:
-      print(prop)
+    for prop in propertiesList:
       for kw in keywords:
-        """ Checks if property is relevant by checking if it contains the
-            following: left, right, margin, padding, background, border, float"""
+        """ Checks if property and values are relevant by checking if it
+            contains the following: left, right, margin, padding, background,
+            border, float """
         if kw in prop:
           relevantProperty = prop.split(':')
           self.properties[relevantProperty[0].strip()] = relevantProperty[1].strip()
 
+  def __str__(self):
+    cssProperties = ''
+    for cssProperty, value in iter(self.properties.items()):
+      cssProperties += '  ' + cssProperty + ':' + value + ';\n'
+    return self.selector + '{\n' + cssProperties + '}'
 
 
 def main():
-  El = Element('.sd > ff {height: 40px; float: left; margin: 4px 4px 5px 10px; }')
-  print(El.selector)
-  print(El.properties)
+  El = Element('.sd > ff {height: 40px; float: left; margin: 4px 4px 5px 10px; padding-left: 40px; position: right }')
+  print(El)
 
 if __name__ == '__main__':
   main()
