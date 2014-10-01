0c64f42320d56b1962f175c836cc507f223d9a70
Husam Younis <youhusam@gmail.com>
2014-10-01 06:39:05 +0300

    add replace option

--- rtlhelper.py
+++ rtlhelper.py
@@ -8,15 +8,15 @@ class Element:
   KEYWORDS = ['left', 'right', 'margin', 'padding',
    'background:', 'position', 'float', 'shadow']
 
-  def __init__(self, rule):
+  def __init__(self, rule, rep):
     for kw in Element.KEYWORDS: #Check if a keyword exists before continuing
       if kw in rule:
         self.selector = ""
         self.properties = OrderedDict()
-        self.populate(rule)
+        self.populate(rule, rep)
         break #if a keyword exists, no need to loop through the rest
 
-  def populate(self, rule):
+  def populate(self, rule, rep):
     """ Populate the properties dict with the relevant properties """
     splittedRule = rule.strip('}').split('{')
     self.selector = splittedRule[0].strip()
@@ -32,12 +32,25 @@ class Element:
           value = relevantProperty[1].strip()
           if 'bottom' in cssProperty or 'top' in cssProperty: break
           if cssProperty == 'padding' or cssProperty == 'margin':
-            if len(value.split(' ')) == 4:
-              if value.split(' ')[1] and value.split(' ')[3] == 0:
+            valueArray = value.split(' ')
+            if len(valueArray) == 4:
+              if valueArray[1] and valueArray[3] == 0:
                 break
+              elif rep:
+                valueArray[1], valueArray[3] = valueArray[3], valueArray[1]
+                value = ' '.join(valueArray)
+
             else: break
+          elif rep:
+            cssProperty = cssProperty.replace('left', 'l-ft')
+            cssProperty = cssProperty.replace('right', 'left')
+            cssProperty = cssProperty.replace('l-ft', 'right')
+            value = value.replace('left', 'l-ft')
+            value = value.replace('right', 'left')
+            value = value.replace('l-ft', 'right')
 
           self.properties[cssProperty] = value
+          break
 
   def __str__(self):
     try:
@@ -51,8 +64,9 @@ class Element:
 def main(argv):
   inputfile = ''
   outputfile = ''
+  rep = False
   try:
-     opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+     opts, args = getopt.getopt(argv,"rhi:o:",["ifile=","ofile="])
   except getopt.GetoptError:
      print ('rtlhelper.py -i <inputfile> -o <outputfile>')
      sys.exit(2)
@@ -60,6 +74,8 @@ def main(argv):
     if opt == '-h':
       print ('rtlhelper.py -i <inputfile> -o <outputfile>')
       sys.exit()
+    elif opt == '-r':
+      rep = True
     elif arg =='-o' or arg == '' or arg == '-i' or opt == '':
         print ('rtlhelper.py -i <inputfile> -o <outputfile>')
         sys.exit(2)
@@ -83,7 +99,7 @@ def main(argv):
     print('Processing...')
     elements = cssFile.split('}')
     for element in elements:
-      el = Element(element + '}')
+      el = Element(element + '}', rep)
       outFile.write(str(el))
     outFile.close()
     print('Done!')
